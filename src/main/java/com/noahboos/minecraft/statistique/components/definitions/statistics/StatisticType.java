43f145cfdc0845160952a7f47c495a53dfbd48ce
Noah Boos <noahboos-dev@proton.me>
2026-04-09 21:02:52 +0200

    Feature - Added first statistic presets to the `StatisticType` enumerator; Added the `getNewStatistic()` method to the enumerator.

--- src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/StatisticType.java
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/StatisticType.java
@@ -3,7 +3,9 @@ package com.noahboos.minecraft.statistique.components.definitions.statistics;
 import net.minecraft.client.resources.language.I18n;
 
 public enum StatisticType {
-    /* Statistic types are declared here. */;
+    USED_DURABILITY("used_durability", "statistique.used_durability"),
+    MINED_BLOCKS("mined_blocks", "statistique.mined_blocks"),
+    STRIPPED_BLOCKS("stripped_blocks", "statistique.stripped_blocks");
 
     private final String name;
     private final String translationKey;
@@ -23,5 +25,9 @@ public enum StatisticType {
 
     public String getTranslation() {
         return I18n.get(translationKey);
+    }
+
+    public Statistic getNewStatistic() {
+        return new Statistic(name, 0);
     }
 }
