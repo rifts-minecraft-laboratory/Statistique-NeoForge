c9e55e04351e5da5dd776d946dc4277f012b13b0
Noah Boos <noahboos-dev@proton.me>
2026-04-12 00:00:42 +0200

    Feature - Implemented the `PickaxeStatistics` data component.

--- src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/tools/items/PickaxeStatistics.java
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/tools/items/PickaxeStatistics.java
@@ -1,7 +1,46 @@
 package com.noahboos.minecraft.statistique.components.definitions.statistics.tools.items;
 
+import com.mojang.serialization.Codec;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.Statistic;
 import com.noahboos.minecraft.statistique.components.definitions.statistics.tools.ToolCore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PickaxeStatistics extends ToolCore {
-    //
+    public static final Codec<PickaxeStatistics> CODEC = Codec.unboundedMap(Codec.STRING, Statistic.CODEC).xmap(PickaxeStatistics::fromMap, PickaxeStatistics::toMap);
+
+    protected PickaxeStatistics(Map<String, Statistic> statistics) {
+        super(statistics);
+    }
+
+    public PickaxeStatistics() {
+        super(getDefaultStatistics());
+    }
+
+    @Override
+    protected PickaxeStatistics create(Map<String, Statistic> statistics) {
+        return new PickaxeStatistics(statistics);
+    }
+
+    private static Map<String, Statistic> getDefaultStatistics() {
+        Map<String, Statistic> statistics = new HashMap<>(new ToolCore().toMap());
+        return statistics;
+    }
+
+    private static PickaxeStatistics fromMap(Map<String, Statistic> map) {
+        Map<String, Statistic> statistics = new HashMap<>(new PickaxeStatistics().statistics);
+        statistics.putAll(map);
+        return new PickaxeStatistics(statistics);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
