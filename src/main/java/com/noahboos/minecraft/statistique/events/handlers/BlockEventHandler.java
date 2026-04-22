4a01890893c58a0cf94a827ab2f93b9ec1ca5a02
Noah Boos <noahboos-dev@proton.me>
2026-04-22 22:14:02 +0200

    Refactor - Removed `onToolModifiedBlock` and `onRightClickBlock` methods from `BlockEventHandler` to "reset" event handling logic.

--- src/main/java/com/noahboos/minecraft/statistique/events/handlers/BlockEventHandler.java
+++ src/main/java/com/noahboos/minecraft/statistique/events/handlers/BlockEventHandler.java
@@ -14,38 +14,6 @@ import java.util.logging.Logger;
 
 public class BlockEventHandler {
     @SubscribeEvent
-    public static void onToolModifiedBlock(BlockEvent.BlockToolModificationEvent event) {
-        if (event.getLevel().isClientSide()) return;
-        if (!(event.getPlayer() instanceof Player player)) return;
-
-        ItemStack mainHandItemStack = player.getMainHandItem();
-        Core statistics = EquipmentComponentMapper.getStatisticsFromItem(mainHandItemStack);
-        if (!(statistics instanceof BlockBreakingToolCore)) return;
-
-        Core updatedStatistics = ActionResolver.resolve(event, statistics);
-        if (updatedStatistics == null) return;
-
-        EquipmentComponentMapper.setStatisticsToItem(mainHandItemStack, updatedStatistics);
-        Logger.getGlobal().info("Statistics has been updated: " + updatedStatistics.toMap().toString());
-    }
-
-    @SubscribeEvent
-    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-        if (event.getLevel().isClientSide()) return;
-
-        Player player = event.getEntity();
-        ItemStack mainHandItemStack = player.getMainHandItem();
-        Core statistics = EquipmentComponentMapper.getStatisticsFromItem(mainHandItemStack);
-        if (!(statistics instanceof BlockBreakingToolCore)) return;
-
-        Core updatedStatistics = ActionResolver.resolve(event, statistics);
-        if (updatedStatistics == null) return;
-
-        EquipmentComponentMapper.setStatisticsToItem(mainHandItemStack, updatedStatistics);
-        Logger.getGlobal().info("Statistics has been updated: " + updatedStatistics.toMap().toString());
-    }
-
-    @SubscribeEvent
     public static void onBrokenBlock(BlockEvent.BreakEvent event) {
         if (event.getLevel().isClientSide()) return;
 
