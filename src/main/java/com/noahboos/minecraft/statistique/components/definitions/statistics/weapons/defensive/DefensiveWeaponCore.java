4634ad38a486fb77d0ca211613eae55d309f82a0
Noah Boos <noahboos-dev@proton.me>
2026-04-22 19:09:15 +0200

    Feature - Added `DefensiveWeaponCore` and `ShieldStatistics` components.

---
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/weapons/defensive/DefensiveWeaponCore.java
@@ -0,0 +1,7 @@
+package com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.defensive;
+
+import com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.WeaponCore;
+
+public class DefensiveWeaponCore extends WeaponCore {
+    //
+}
---
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/weapons/defensive/items/ShieldStatistics.java
@@ -0,0 +1,7 @@
+package com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.defensive.items;
+
+import com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.defensive.DefensiveWeaponCore;
+
+public class ShieldStatistics extends DefensiveWeaponCore {
+    //
+}
