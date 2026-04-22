8f26f69e765a6f701ec72b7c53f6851ae233a7ce
Noah Boos <noahboos-dev@proton.me>
2026-04-22 22:13:45 +0200

    Refactor - Removed `EntityEventHandler` and unregistered it from the event bus.

--- src/main/java/com/noahboos/minecraft/statistique/Statistique.java
+++ src/main/java/com/noahboos/minecraft/statistique/Statistique.java
@@ -2,7 +2,6 @@ package com.noahboos.minecraft.statistique;
 
 import com.noahboos.minecraft.statistique.components.DataComponents;
 import com.noahboos.minecraft.statistique.events.handlers.BlockEventHandler;
-import com.noahboos.minecraft.statistique.events.handlers.EntityEventHandler;
 import com.noahboos.minecraft.statistique.events.handlers.ItemEventHandler;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
@@ -22,7 +21,6 @@ public class Statistique {
     public Statistique(IEventBus modEventBus, ModContainer modContainer) {
         DataComponents.COMPONENTS.register(modEventBus);
         NeoForge.EVENT_BUS.register(BlockEventHandler.class);
-        NeoForge.EVENT_BUS.register(EntityEventHandler.class);
         NeoForge.EVENT_BUS.register(ItemEventHandler.class);
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
