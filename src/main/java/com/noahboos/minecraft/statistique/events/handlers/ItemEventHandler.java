fb818e11bef6846f954ac8d8f731f76d05eabdb6
Noah Boos <noahboos-dev@proton.me>
2026-04-25 22:32:22 +0200

    Feature - Added statistics handling for item pickup event in `ItemEventHandler`.

--- src/main/java/com/noahboos/minecraft/statistique/events/handlers/ItemEventHandler.java
+++ src/main/java/com/noahboos/minecraft/statistique/events/handlers/ItemEventHandler.java
@@ -3,7 +3,10 @@ package com.noahboos.minecraft.statistique.events.handlers;
 import com.noahboos.minecraft.statistique.components.utils.statistics.EquipmentComponentMapper;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+import java.util.logging.Logger;
 
 public class ItemEventHandler {
     @SubscribeEvent
@@ -12,5 +15,16 @@ public class ItemEventHandler {
 
         ItemStack craftedItemStack = event.getCrafting();
         EquipmentComponentMapper.setStatisticsToItem(craftedItemStack, null);
+
+        Logger.getGlobal().info("Statistics has been added: " + craftedItemStack.getDisplayName().getString());
+    }
+
+    @SubscribeEvent
+    public static void onItemPickedUp(ItemEntityPickupEvent.Post event) {
+        if (event.getPlayer().level().isClientSide()) return;
+
+        event.getPlayer().getInventory().forEach(pickedUpItem -> EquipmentComponentMapper.setStatisticsToItem(pickedUpItem, null));
+
+        Logger.getGlobal().info("Statistics has been added to all items in inventory.");
     }
 }
