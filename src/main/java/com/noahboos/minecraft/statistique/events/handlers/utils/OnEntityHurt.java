44192b5445519139ca55bc5529c06c75e976b65c
Noah Boos <noahboos-dev@proton.me>
2026-04-26 15:17:16 +0200

    Refactor - Refactored entity damage handling by extracting attacker processing into a new OnEntityHurt class for improved code organisation.

---
+++ src/main/java/com/noahboos/minecraft/statistique/events/handlers/utils/OnEntityHurt.java
@@ -0,0 +1,33 @@
+package com.noahboos.minecraft.statistique.events.handlers.utils;
+
+import com.noahboos.minecraft.statistique.components.definitions.statistics.Core;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.offensive.OffensiveWeaponCore;
+import com.noahboos.minecraft.statistique.components.utils.statistics.EquipmentComponentMapper;
+import com.noahboos.minecraft.statistique.events.resolvers.ActionResolver;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+
+import java.util.logging.Logger;
+
+public class OnEntityHurt {
+    public static void processAttacker(LivingDamageEvent.Post event) {
+        if (!(event.getSource().getEntity() instanceof LivingEntity attacker)) return;
+
+        ItemStack mainHandItemStack = attacker.getMainHandItem();
+        Core<?> mainHandStatistics = EquipmentComponentMapper.getStatisticsFromItem(mainHandItemStack);
+
+        if (mainHandStatistics instanceof OffensiveWeaponCore<?>) {
+            Core<?> updatedStatistics = ActionResolver.resolve(event, mainHandStatistics);
+            if (updatedStatistics != null) {
+                EquipmentComponentMapper.setStatisticsToItem(mainHandItemStack, updatedStatistics);
+
+                Logger.getGlobal().info("Attacker's main hand statistics has been updated: " + updatedStatistics.toMap().toString());
+            }
+        }
+    }
+
+    public static void processVictim(LivingDamageEvent.Post event) {
+        // Victim processing will be implemented in this method, yet it is empty. :0
+    }
+}
