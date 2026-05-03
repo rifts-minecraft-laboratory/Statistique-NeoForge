d43b534d68e26cb7d00b9b8ec4e6c23dc021b835
Noah Boos <noahboos-dev@proton.me>
2026-05-03 02:27:24 +0200

    Feature - Added new statistics for arrows: hit, missed and total shot arrows.

--- src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/StatisticType.java
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/StatisticType.java
@@ -4,11 +4,14 @@ public enum StatisticType {
     BROKEN_BLOCKS("broken_blocks"),
     BLOCKED_DAMAGES("blocked_damages"),
     DEALT_DAMAGES("dealt_damages"),
+    HIT_ARROWS("hit_arrows"),
     KILLED_ENTITIES("killed_entities"),
     KILLED_HOSTILE_ENTITIES("killed_hostile_entities"),
     KILLED_NEUTRAL_ENTITIES("killed_neutral_entities"),
     KILLED_PASSIVE_ENTITIES("killed_passive_entities"),
-    MITIGATED_DAMAGES("mitigated_damages"),;
+    MISSED_ARROWS("missed_arrows"),
+    MITIGATED_DAMAGES("mitigated_damages"),
+    TOTAL_SHOT_ARROWS("total_shot_arrows"),;
 
     private final String name;
 
--- src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/weapons/offensive/ranged/RangedOffensiveWeaponCore.java
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/weapons/offensive/ranged/RangedOffensiveWeaponCore.java
@@ -1,6 +1,7 @@
 package com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.offensive.ranged;
 
 import com.noahboos.minecraft.statistique.components.definitions.statistics.Statistic;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.StatisticType;
 import com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.offensive.OffensiveWeaponCore;
 
 import java.util.HashMap;
@@ -18,7 +19,9 @@ public abstract class RangedOffensiveWeaponCore<T extends RangedOffensiveWeaponC
     @Override
     protected Map<String, Statistic> getDefaultStatistics() {
         Map<String, Statistic> statistics = new HashMap<>(super.getDefaultStatistics());
-        // Common statistics of the ranged offensive weapon will be added here. None for now. :3
+        statistics.put(StatisticType.HIT_ARROWS.getName(), StatisticType.HIT_ARROWS.getNewStatistic());
+        statistics.put(StatisticType.MISSED_ARROWS.getName(), StatisticType.MISSED_ARROWS.getNewStatistic());
+        statistics.put(StatisticType.TOTAL_SHOT_ARROWS.getName(), StatisticType.TOTAL_SHOT_ARROWS.getNewStatistic());
         return statistics;
     }
 }
