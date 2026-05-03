21e333f0fc32983fe6b2f3bb2ea6fa128027f85f
Noah Boos <noahboos-dev@proton.me>
2026-05-03 12:13:48 +0200

    Feature - Added arrow statistic treatments.

--- src/main/java/com/noahboos/minecraft/statistique/events/resolvers/OnProjectileImpactResolver.java
+++ src/main/java/com/noahboos/minecraft/statistique/events/resolvers/OnProjectileImpactResolver.java
@@ -1,11 +1,25 @@
 package com.noahboos.minecraft.statistique.events.resolvers;
 
 import com.noahboos.minecraft.statistique.components.definitions.statistics.Core;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.StatisticType;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.offensive.ranged.RangedOffensiveWeaponCore;
+import net.minecraft.world.phys.HitResult;
 import net.neoforged.neoforge.event.entity.ProjectileImpactEvent;
 
 public class OnProjectileImpactResolver {
     public static Core<?> resolveProjectileImpact(ProjectileImpactEvent event, Core<?> statistics) {
-        // Behaviour will be implemented here.
-        return statistics;
+        if (!(statistics instanceof RangedOffensiveWeaponCore<?> _statistics)) return statistics;
+
+        _statistics = _statistics.incrementStatistic(StatisticType.TOTAL_SHOT_ARROWS.getName(), 1);
+
+        HitResult hitResult = event.getRayTraceResult();
+
+        if (hitResult.getType() == HitResult.Type.ENTITY) {
+            _statistics = _statistics.incrementStatistic(StatisticType.HIT_ARROWS.getName(), 1);
+        } else if (hitResult.getType() == HitResult.Type.BLOCK) {
+            _statistics = _statistics.incrementStatistic(StatisticType.MISSED_ARROWS.getName(), 1);
+        }
+
+        return _statistics;
     }
 }
