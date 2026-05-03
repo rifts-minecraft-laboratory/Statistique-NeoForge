bd2d3b6d3edd7787eb3e4b355778a8b9288e63c9
Noah Boos <noahboos-dev@proton.me>
2026-05-03 15:00:26 +0200

    Feature - Added new statistics for tridents: hit, missed and total thrown tridents.

--- src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/StatisticType.java
+++ src/main/java/com/noahboos/minecraft/statistique/components/definitions/statistics/StatisticType.java
@@ -5,13 +5,16 @@ public enum StatisticType {
     BLOCKED_DAMAGES("blocked_damages"),
     DEALT_DAMAGES("dealt_damages"),
     HIT_ARROWS("hit_arrows"),
+    HIT_TRIDENTS("hit_tridents"),
     KILLED_ENTITIES("killed_entities"),
     KILLED_HOSTILE_ENTITIES("killed_hostile_entities"),
     KILLED_NEUTRAL_ENTITIES("killed_neutral_entities"),
     KILLED_PASSIVE_ENTITIES("killed_passive_entities"),
     MISSED_ARROWS("missed_arrows"),
+    MISSED_TRIDENTS("missed_tridents"),
     MITIGATED_DAMAGES("mitigated_damages"),
-    TOTAL_SHOT_ARROWS("total_shot_arrows"),;
+    TOTAL_SHOT_ARROWS("total_shot_arrows"),
+    TOTAL_THROWN_TRIDENTS("total_thrown_tridents");
 
     private final String name;
 
