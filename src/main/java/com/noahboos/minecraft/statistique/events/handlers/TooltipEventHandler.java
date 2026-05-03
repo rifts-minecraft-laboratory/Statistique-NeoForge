f25232a095a35e6b8e9c511924fb508fb247ed2e
Noah Boos <noahboos-dev@proton.me>
2026-05-03 02:02:37 +0200

    Feature - Sort the statistics alphabetically in the tooltip by using a `TreeMap<>()` instead of a classic `Map<>()`.

--- src/main/java/com/noahboos/minecraft/statistique/events/handlers/TooltipEventHandler.java
+++ src/main/java/com/noahboos/minecraft/statistique/events/handlers/TooltipEventHandler.java
@@ -1,6 +1,7 @@
 package com.noahboos.minecraft.statistique.events.handlers;
 
 import com.noahboos.minecraft.statistique.components.definitions.statistics.Core;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.Statistic;
 import com.noahboos.minecraft.statistique.components.definitions.statistics.StatisticType;
 import com.noahboos.minecraft.statistique.components.utils.statistics.EquipmentComponentMapper;
 import net.minecraft.network.chat.Component;
@@ -9,6 +10,8 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class TooltipEventHandler {
     @SubscribeEvent
@@ -21,7 +24,9 @@ public class TooltipEventHandler {
             tooltip.add(Component.empty());
             tooltip.add(Component.translatable("statistiques.tooltip.title"));
 
-            statistics.toMap().forEach((key, value) -> {
+            Map<String, Statistic> _statistics = new TreeMap<>(statistics.toMap());
+
+            _statistics.forEach((key, value) -> {
                 StatisticType statisticType = StatisticType.valueOf(key.toUpperCase());
                 tooltip.add(Component.translatable(statisticType.getTranslationKey(), value.value()));
             });
