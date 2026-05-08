4c63eda7516479a46e76a0129cbeaae67184e0d7
Noah Boos <noahboos-dev@proton.me>
2026-05-08 19:07:32 +0200

    Refactor - Fused `OnProjectileImpact` in `OnProjectileImpactHandler`.

--- src/main/java/com/noahboos/minecraft/statistique/events/handlers/OnProjectileImpactHandler.java
+++ src/main/java/com/noahboos/minecraft/statistique/events/handlers/OnProjectileImpactHandler.java
@@ -1,6 +1,12 @@
 package com.noahboos.minecraft.statistique.events.handlers;
 
-import com.noahboos.minecraft.statistique.events.utils.OnProjectileImpact;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.Core;
+import com.noahboos.minecraft.statistique.components.definitions.statistics.weapons.offensive.OffensiveWeaponCore;
+import com.noahboos.minecraft.statistique.components.utils.statistics.EquipmentComponentMapper;
+import com.noahboos.minecraft.statistique.events.resolvers.OnProjectileImpactResolver;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TridentItem;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.ProjectileImpactEvent;
 
@@ -13,6 +19,32 @@ public class OnProjectileImpactHandler {
     }
 
     private static void handle(ProjectileImpactEvent event) {
-        OnProjectileImpact.processProjectileImpact(event);
+        processProjectileImpact(event);
+    }
+
+    private static void processProjectileImpact(ProjectileImpactEvent event) {
+        if (!(event.getProjectile().getOwner() instanceof LivingEntity projectileOwner)) return;
+
+        ItemStack weapon = event.getProjectile().getWeaponItem();
+
+        if (weapon.getItem() instanceof TridentItem) {
+            processItemStack(event, weapon, weapon);
+            return;
+        }
+
+        processItemStack(event, weapon, projectileOwner.getMainHandItem());
+        processItemStack(event, weapon, projectileOwner.getOffhandItem());
+    }
+
+    private static void processItemStack(ProjectileImpactEvent event, ItemStack weapon, ItemStack itemToProcess) {
+        if (weapon == ItemStack.EMPTY || !weapon.getItem().equals(itemToProcess.getItem())) return;
+
+        Core<?> statistics = EquipmentComponentMapper.getStatisticsFromItem(itemToProcess);
+        if (!(statistics instanceof OffensiveWeaponCore<?>)) return;
+
+        Core<?> _statistics = OnProjectileImpactResolver.resolveProjectileImpact(event, statistics);
+        if (_statistics == null) return;
+
+        EquipmentComponentMapper.setStatisticsToItem(itemToProcess, _statistics);
     }
 }
